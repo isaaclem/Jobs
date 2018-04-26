8aab7e5e8ef423e5427c8516c21220e858a64580
isaaclem <isaaclem@Isaacs-MacBook-Air.local>
2018-04-26 23:20:27 +0800

    Initial commit.

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
+import ReviewScreen from './screens/ReviewScreen';
+import SettingsScreen from './screens/SettingsScreen';
+
+const MainNavigator = TabNavigator({
+  welcome: { screen: WelcomeScreen },
+  auth: { screen: AuthScreen },
+  main: {
+    screen: TabNavigator({
+      map: { screen: MapScreen },
+      deck: { screen: DeckScreen },
+      review: {
+        screen: StackNavigator({
+          review: { screen: ReviewScreen },
+          settings: { screen: SettingsScreen }
+        })
+      }
+    })
+  }
+});
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <MainNavigator />
+    );
+  }
+}

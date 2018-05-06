ffa0b860a8120c01ca28d5c5ffa1de20e4d13461
isaaclem <isaaclem@Isaacs-MacBook-Air.local>
2018-05-06 23:57:47 +0800

    added qs and latlng-to-zip barebone implementation

---
+++ actions/job_actions.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import reverseGeocode from 'latlng-to-zip';
+import qs from 'qs';
+
+import {
+  FETCH_JOBS
+} from './types';
+
+const JOB_QUERY_PARAMS = {
+  publisher: '4201738803816157',
+  format: 'json',
+  v: 2,
+  latlong: 1,
+  radius: 10,
+  q: 'javascript'
+};
+
+export const fetchJobs = region => async dispatch => {
+  try {
+    let zipCode = await reverseGeocode(region);
+  } catch (err) {
+    console.log(err);
+  }
+};

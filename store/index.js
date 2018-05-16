7b86a99f3f2c6480636884c88ea12e9643f65e84
isaaclem <lem.chiew.choon@gmail.com>
2018-05-17 00:28:25 +0800

    added redux-persist

--- store/index.js
+++ store/index.js
@@ -1,8 +1,8 @@
 import { compose, applyMiddleware } from 'redux';
 import Reactotron from 'reactotron-react-native';
 import thunk from 'redux-thunk';
-//import { persistStore, autoRehydrate } from 'redux-persist';
-//import { AsyncStorage } from 'react-native';
+import { persistStore, autoRehydrate } from 'redux-persist';
+import { AsyncStorage } from 'react-native';
 import reducers from '../reducers';
 import '../ReactotronConfig';
 
@@ -10,8 +10,10 @@ const store = Reactotron.createStore(
   reducers,
   {},
   compose(
-    applyMiddleware(thunk)
+    applyMiddleware(thunk),
+    autoRehydrate()
   )
 );
 
+persistStore(store, { storage: AsyncStorage, whitelist: ['likedJobs'] });
 export default store;
